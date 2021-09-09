35b2b9fbfae75ebc609d1c3f37eac6770226aeac
rajdeep <rajdeep.gautam@gmail.com>
2021-09-09 16:20:27 +0530

    "swagger commit"

--- SpringUserLogin/src/main/java/com/user/jwt/SecurityConfigurer.java
+++ SpringUserLogin/src/main/java/com/user/jwt/SecurityConfigurer.java
@@ -2,12 +2,13 @@ package com.user.jwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.authentication.AuthenticationManagerBeanDefinitionParser;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -40,8 +41,36 @@ class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 		return super.authenticationManagerBean();
 		
 	}
+	    @Override
+	    public void configure(WebSecurity web) throws Exception {
+	        web.ignoring().antMatchers("/v2/api-docs",
+	                                   "/configuration/ui",
+	                                   "/swagger-resources/**",
+	                                   "/configuration/security",
+	                                   "/swagger-ui.html",
+	                                   "/webjars/**");
+	    }
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
+	/*
+	 * @Override protected void configure(HttpSecurity httpSecurity) throws
+	 * Exception {
+	 * 
+	 * 
+	 * 
+	 * httpSecurity.csrf().disable().authorizeRequests()
+	 * .antMatchers("/authenticate", "/authenticate-custom-token",
+	 * "http://localhost:8761/","one "more swagger url entry).permitAll()
+	 * .anyRequest().authenticated().and().exceptionHandling().and().
+	 * sessionManagement() .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	 * httpSecurity.addFilterBefore(jwtRequestFilter,
+	 * UsernamePasswordAuthenticationFilter.class);
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
 }
